1883ade09220f71de0c0400c6bd58a3c8e8615dc
hulyalasch <hulyalasch@gmail.com>
2023-04-15 19:49:46 +0200

    Updates

---
+++ _app/immutable/chunks/1.b64706f1.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.1690d4ed.js";export{t as component};
--- _app/immutable/chunks/2.4d5ef956.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.d5ac9949.js";export{t as component};
